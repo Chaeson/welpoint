b18ee8a6d72c23101578c19bb2d46df50f897163
Chae <chae@ubcn.co.kr>
2020-11-30 10:54:48 +0900

    Init Commit

---
+++ src/main/java/kr/co/ubcn/welpoint/WelpointApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ubcn.welpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelpointApplication.class, args);
+    }
+
+}
